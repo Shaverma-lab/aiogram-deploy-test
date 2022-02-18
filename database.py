39e2b4038bed0a65d847c03721d8b8dfeb82e37b
Олег Никитин <olegn3514@gmail.com>
2022-02-18 10:50:31 +0200

    first deploy

--- database.py
+++ database.py
@@ -8,15 +8,11 @@ class Database:
         self.cur = self.conn.cursor()
 
     def load(self, user_id):
-        self.cur.execute(f"""
-            SELECT id FROM test_db WHERE user_id = {user_id}
-        """)
+        self.cur.execute(f"SELECT id FROM test_db WHERE user_id = {user_id}")
 
         return self.cur.fetchone()
 
     def add_new_user(self, user_id):
-        self.cur.execute(f"""
-            INSERT INTO test_db(user_id) VALUES(%s);
-        """, (user_id))
+        self.cur.execute(f"INSERT INTO test_db(user_id) VALUES({user_id})")
 
         self.conn.commit()
