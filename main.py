4c96955e13b5c05b11fe8e08787c260ab875fe00
Олег Никитин <olegn3514@gmail.com>
2022-02-18 09:33:29 +0200

    first deploy

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
 @dp.message_handler()
 async def echo(message: types.Message):
