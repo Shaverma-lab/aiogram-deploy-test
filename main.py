b87cc3e373a644044b070b804ec8ef56cbccecad
Олег Никитин <olegn3514@gmail.com>
2022-02-18 12:17:02 +0200

    first deploy

--- main.py
+++ main.py
@@ -26,11 +26,11 @@ db_object = db_connection.cursor()
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    db_object.execute(f"SELECT id FROM test_db WHERE user_id = {message.chat.id}")
+    db_object.execute(f"SELECT user_id FROM test_db WHERE user_id = {message.chat.id}")
     result = db_object.fetchone()
 
     if not result:
-        db_object.execute(f"INSERT INTO test_db(user_id) VALUES ({message.chat.id})")
+        db_object.execute(f"INSERT INTO test_db(user_id) VALUES (%s)", (message.chat.id,))
         db_connection.commit()
 
     await bot.send_message(message.chat.id, message.text)
